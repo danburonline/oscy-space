58f9baf4897fcc41eaa56f9c0cb3aac309ac2c1a
Daniel Burger <public@danielburger.online>
2021-05-19 22:31:39 +0200

    Small fix

--- components/molecules/Card.tsx
+++ components/molecules/Card.tsx
@@ -9,7 +9,7 @@ export default function Card(props: CardProps) {
   return (
     <a
       className={
-        'tw-justify-center tw-align-center tw-flex-col tw-self-center tw-justify-self-center tw-border border-grey-600 tw-rounded-md tw-p-8 tw-min-w-full tw-h-full hover:tw-border-blue-600 hover:tw-text-blue-600 tw-flex'
+        'tw-justify-center tw-flex tw-flex-col tw-self-center tw-justify-self-center tw-border tw-border-grey-600 tw-rounded-md tw-p-8 tw-min-w-full hover:tw-border-blue-600 hover:tw-text-blue-600'
       }
       href={props.isNotDone ? '' : props.link}>
       <h2 className='tw-text-xl tw-font-bold'>{props.title} &rarr;</h2>
