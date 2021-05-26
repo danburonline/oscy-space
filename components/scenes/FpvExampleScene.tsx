02edd5d6f5e8ca68019af9f1e30a34482bdf51e8
Daniel Burger <public@danielburger.online>
2021-05-26 20:36:07 +0200

    Adding shadows and basic lights

--- components/scenes/FpvExampleScene.tsx
+++ components/scenes/FpvExampleScene.tsx
@@ -6,14 +6,14 @@ import { Player } from '../objects/complex/Player'
 function SimplePhysicsCube() {
   const [ref] = useBox(() => ({ type: 'Static', position: [0, 0.5, 0] }))
   return (
-    <mesh ref={ref}>
+    <mesh ref={ref} receiveShadow castShadow>
       <boxBufferGeometry args={[1, 1, 1]} />
-      <meshBasicMaterial color='white' />
+      <meshStandardMaterial color='white' />
     </mesh>
   )
 }
 
-function WireframePlane() {
+function Ground() {
   const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0] }))
 
   return (
@@ -30,10 +30,11 @@ export default function FpvExample() {
       <PointerLockControls />
       <Sky sunPosition={[100, 10, 100]} />
       <ambientLight intensity={0.3} />
+      <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
 
       <Physics gravity={[0, -30, 0]}>
         <SimplePhysicsCube />
-        <WireframePlane />
+        <Ground />
         <Player />
       </Physics>
     </Canvas>
