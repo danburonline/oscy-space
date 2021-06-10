3c033999c7eae301e845a1e1fb7965cf4adf9b98
DavidEFritz <david.fritz@gmx.ch>
2021-06-10 19:57:15 +0200

    Fixing context lost error

--- components/objects/sound/AmbientSoundObject.tsx
+++ components/objects/sound/AmbientSoundObject.tsx
@@ -122,5 +122,5 @@ export default function SoundObject(props: SoundObjectProps) {
       </mesh>
     )
   })
-  return <>{PositionalSoundObject}</>
+  return <Suspense fallback={null}>{PositionalSoundObject}</Suspense>
 }
