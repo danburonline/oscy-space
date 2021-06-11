91a0963d3f2fbec8a5b60f5651960f7bc6a9ecd6
Daniel Burger <public@danielburger.online>
2021-06-11 16:59:50 +0200

    Adding yet another try to solve the WAV problem

--- components/objects/sound/AmbientSoundObject.tsx
+++ components/objects/sound/AmbientSoundObject.tsx
@@ -5,7 +5,14 @@ import { useThree, useLoader } from '@react-three/fiber'
 import { PositionalAudioHelper } from 'three/examples/jsm/helpers/PositionalAudioHelper.js'
 import { useControls } from 'leva'
 
-function PositionalAudio({ refs, url, volume, rolloffFactor, coneOuterGain, state }) {
+function PositionalAudio({
+  refs,
+  url,
+  volume,
+  rolloffFactor,
+  coneOuterGain,
+  state
+}) {
   const sound = refs
   const { camera } = useThree()
   const [listener] = useState(() => new THREE.AudioListener())
@@ -29,11 +36,11 @@ function PositionalAudio({ refs, url, volume, rolloffFactor, coneOuterGain, stat
     sound.current.setRolloffFactor(rolloffFactor)
     sound.current.setVolume(volume)
   }, [rolloffFactor, volume, coneOuterGain])
-  if(state) {
+  if (state) {
     useEffect(() => {
       sound.current.play()
     }, [state])
-  }else{
+  } else {
     useEffect(() => {
       sound.current.pause()
     }, [state])
@@ -57,8 +64,6 @@ type SoundObjectProps = {
 }
 
 export default function SoundObject(props: SoundObjectProps) {
-  
-
   const audioRefs = useMemo(
     () =>
       Array(props.soundObjects.length)
@@ -68,43 +73,45 @@ export default function SoundObject(props: SoundObjectProps) {
   )
 
   const PositionalSoundObject = props.soundObjects.map((soundObject, index) => {
-
     const name = soundObject.name
 
-    const { Rotation, Volume, Rolloff, X, Y, Z, ConeOuterGain } = useControls( name, {
-      Rotation: {
-        value: soundObject.rotation,
-        min: 0,
-        max: Math.PI * 2,
-        step: Math.PI * 0.25
-      },
-      Volume: {
-        value: 1,
-        min: 0,
-        max: 1,
-        step: 0.05
-      },
-      Rolloff: {
-        value: 1,
-        min: 0,
-        max: 1,
-        step: 0.05
-      },
-      ConeOuterGain: {
-        value: 0,
-        min: 0,
-        max: 1
-      },
-      X: {
-        value: soundObject.x,
-      },
-      Y: {
-        value: soundObject.y,
-      },
-      Z: {
-        value: soundObject.z,
+    const { Rotation, Volume, Rolloff, X, Y, Z, ConeOuterGain } = useControls(
+      name,
+      {
+        Rotation: {
+          value: soundObject.rotation,
+          min: 0,
+          max: Math.PI * 2,
+          step: Math.PI * 0.25
+        },
+        Volume: {
+          value: 1,
+          min: 0,
+          max: 1,
+          step: 0.05
+        },
+        Rolloff: {
+          value: 1,
+          min: 0,
+          max: 1,
+          step: 0.05
+        },
+        ConeOuterGain: {
+          value: 0,
+          min: 0,
+          max: 1
+        },
+        X: {
+          value: soundObject.x
+        },
+        Y: {
+          value: soundObject.y
+        },
+        Z: {
+          value: soundObject.z
+        }
       }
-    })
+    )
 
     return (
       <mesh position={[X, Y, Z]} rotation={[0, Rotation, 0]}>
--- components/scenes/AmbientSound8ChannelSceneWav.tsx
+++ components/scenes/AmbientSound8ChannelSceneWav.tsx
@@ -14,7 +14,7 @@ const soundObjects = [
     y: 0,
     z: -10,
     position: [10, 0, -10],
-    filePath: '/sounds/example-from-the-web.wav',
+    filePath: 'https://storage.googleapis.com/oscy-audio/test.wav',
     name: 'r',
     rotation: 0
   },
@@ -24,7 +24,7 @@ const soundObjects = [
     y: 0,
     z: 10,
     position: [10, 0, 10],
-    filePath: '/sounds/example-from-the-web.wav',
+    filePath: 'https://storage.googleapis.com/oscy-audio/test.wav',
     name: 'rs',
     rotation: Math.PI
   },
@@ -35,7 +35,7 @@ const soundObjects = [
     y: 10,
     z: -10,
     position: [10, 10, -10],
-    filePath: '/sounds/example-from-the-web.wav',
+    filePath: 'https://storage.googleapis.com/oscy-audio/test.wav',
     name: 'hr',
     rotation: 0
   },
@@ -46,7 +46,7 @@ const soundObjects = [
     y: 10,
     z: 10,
     position: [10, 10, 10],
-    filePath: '/sounds/example-from-the-web.wav',
+    filePath: 'https://storage.googleapis.com/oscy-audio/test.wav',
     name: 'hrs',
     rotation: Math.PI
   },
@@ -57,7 +57,7 @@ const soundObjects = [
     y: 0,
     z: -10,
     position: [-10, 0, -10],
-    filePath: '/sounds/example-from-the-web.wav',
+    filePath: 'https://storage.googleapis.com/oscy-audio/test.wav',
     name: 'l',
     rotation: 0
   },
@@ -68,7 +68,7 @@ const soundObjects = [
     y: 0,
     z: 10,
     position: [-10, 0, 10],
-    filePath: '/sounds/example-from-the-web.wav',
+    filePath: 'https://storage.googleapis.com/oscy-audio/test.wav',
     name: 'ls',
     rotation: Math.PI
   },
@@ -79,7 +79,7 @@ const soundObjects = [
     y: 10,
     z: -10,
     position: [-10, 10, -10],
-    filePath: '/sounds/example-from-the-web.wav',
+    filePath: 'https://storage.googleapis.com/oscy-audio/test.wav',
     name: 'hl',
     rotation: 0
   },
@@ -89,7 +89,7 @@ const soundObjects = [
     y: 10,
     z: 10,
     position: [-10, 10, 10],
-    filePath: '/sounds/example-from-the-web.wav',
+    filePath: 'https://storage.googleapis.com/oscy-audio/test.wav',
     name: 'hls',
     rotation: Math.PI
   }
