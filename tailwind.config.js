c31ea33b9f30676b5231369296276efd4b672438
Daniel Burger <public@danielburger.online>
2021-07-30 19:48:30 +0200

    Fixing the Tailwind errorr

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ module.exports = {
   purge: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     backgroundColor: theme => ({
       ...theme('colors'),
