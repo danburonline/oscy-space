f203452be77fb6e38ab5fb32600e8287f36cc9b8
Daniel Burger <public@danielburger.online>
2021-08-02 12:09:18 +0200

    Adding tooltips to every scene UI button

--- src/components/AudioButton.tsx
+++ src/components/AudioButton.tsx
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faVolumeUp } from '@fortawesome/free-solid-svg-icons'
 import { faVolumeMute } from '@fortawesome/free-solid-svg-icons'
 import type { AudioButtonProps } from '../types/types'
+import ReactTooltip from 'react-tooltip'
 
 export default function AudioButton(props: AudioButtonProps): JSX.Element {
   const audioButtonOn = <FontAwesomeIcon icon={faVolumeUp} />
@@ -10,12 +11,16 @@ export default function AudioButton(props: AudioButtonProps): JSX.Element {
   const titleText = props.state ? 'Turn audio off' : 'Turn audio on'
 
   return (
-    <button
-      title={titleText}
-      onClick={props.onClick}
-      className='absolute bottom-0 right-0 z-10 flex items-center justify-center p-3 m-3 text-white transition-colors ease-in-out rounded-md cursor-pointer bg-primary duration-350 hover:bg-hover'
-    >
-      {buttonText}
-    </button>
+    <>
+      <button
+        title={titleText}
+        data-tip={titleText}
+        onClick={props.onClick}
+        className='absolute bottom-0 right-0 z-10 flex items-center justify-center p-3 m-3 text-white transition-colors ease-in-out rounded-md cursor-pointer bg-primary duration-350 hover:bg-hover'
+      >
+        {buttonText}
+      </button>
+      <ReactTooltip />
+    </>
   )
 }
