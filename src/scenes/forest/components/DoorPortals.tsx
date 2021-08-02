2e5c4c5c809c3d82a2fa968384e6956bc647f247
Daniel Burger <public@danielburger.online>
2021-08-02 15:34:48 +0200

    Hot fix 2nd door portal in the Forest room

--- src/scenes/forest/components/DoorPortals.tsx
+++ src/scenes/forest/components/DoorPortals.tsx
@@ -1,5 +1,4 @@
 import DoorPortal from '../../../components/DoorPortal'
-import { MathUtils } from 'three'
 
 export default function DoorPortals(): JSX.Element {
   return (
@@ -16,10 +15,10 @@ export default function DoorPortals(): JSX.Element {
         }}
       />
       <DoorPortal
-        position={[8.851, 5.124, -12.527]}
+        position={[8.804, 5.582, -13.659]}
         scale={[0, 0, 0]}
-        size={[0.482, 2.134, 1]}
-        rotation={[0, MathUtils.degToRad(90), 0]}
+        size={[1.719, 3.022, 0.971]}
+        rotation={[0, 0, 0]}
         color={'red'}
         onCollide={() => {
           // eslint-disable-next-line no-undef
